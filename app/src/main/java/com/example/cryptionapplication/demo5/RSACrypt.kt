f3b55e4d0d296e32164929f79ca7b03d9970c34d
GuoJinYi819 <gjy2000@>
2020-05-12 13:45:10 +0800

    0512-分段解密

--- app/src/main/java/com/example/cryptionapplication/demo5/RSACrypt.kt
+++ app/src/main/java/com/example/cryptionapplication/demo5/RSACrypt.kt
@@ -15,7 +15,8 @@ import javax.crypto.Cipher
  *
  */
 object RSACrypt{
-    val MAX_SIZE = 117 //私钥最大加密长度
+    val MAX_SIZE = 117 //加密 最大加密长度
+    val DECRYPT_MAX_SIZE = 128; //解密 最大长度
     val transformation = "RSA"
     //私钥加密    原文           私钥
     fun encryptPrivate(input:String,privateKey: PrivateKey): String {
@@ -65,6 +66,40 @@ object RSACrypt{
         val encrypt = cipher.doFinal(input.toByteArray())
         return Base64.encode(encrypt)
     }
+    // 分段解密       密文
+    fun decryptPrivate(input:String,privateKey: PublicKey): String {
+        var cipher:Cipher = Cipher.getInstance(transformation)
+        //2
+        cipher.init(Cipher.DECRYPT_MODE,privateKey)
+
+        //分段接密
+        val toByteArray = input.toByteArray()
+        var temp:ByteArray? = null
+        var offset = 0  //移动位置
+
+        //字节输出流
+        val bos = ByteArrayOutputStream()
+
+        while (toByteArray.size - offset>0) {
+            //每次最大加密128个字节
+            if (toByteArray.size - offset >= DECRYPT_MAX_SIZE) {
+                //剩余部分大于128
+                //加密完成128  一块
+                temp = cipher.doFinal(toByteArray, offset, DECRYPT_MAX_SIZE)
+                //重新计算偏移位置
+                offset += DECRYPT_MAX_SIZE
+            } else {
+                //加密的最后一块
+                temp = cipher.doFinal(toByteArray, offset, toByteArray.size - offset)
+                offset = toByteArray.size
+            }
+            //临时缓冲区
+            bos.write(temp)
+        }
+        bos.close()
+        //返回
+        return String(bos.toByteArray())
+    }
 
 }
 
@@ -80,11 +115,15 @@ fun main() {
    // println("公钥"+Base64.encode(publicKey.encoded))
    // println("私钥"+Base64.encode(privateKey.encoded))
 
-    var input = "嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿"
-    //私钥加密
+    var input = "嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿嘿"
+    //分段私钥加密
     val encrypt = RSACrypt.encryptPrivate(input, privateKey)
-    println("私钥="+encrypt)
-   // val encryptPublic = RSACrypt.encryptPublic(input, publicKey)
+    println("私钥加密="+encrypt)
+
+    //分段解密
+    val decryptPrivate = RSACrypt.decryptPrivate(encrypt, publicKey)
+    println("公钥解密="+decryptPrivate)
+    // val encryptPublic = RSACrypt.encryptPublic(input, publicKey)
  //   println("共钥="+encryptPublic)
 
 
